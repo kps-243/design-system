bbdd9c1b5fdbbaee9148dfd3bca04be3d3ce65b0
Morgan Kpassi <>
2025-01-06 15:57:59 +0100

    add font expo font

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
-  return (
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  })
+  return fontsLoaded ? (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontFamily: "Roboto-Bold"}}>Open up App.js to start working on your app!</Text>
+      <Text style={{fontFamily: "Roboto-Regular"}}>Open up App.js to start working on your app!</Text>
+
       <StatusBar style="auto" />
     </View>
-  );
+  ) : null;
 }
 
 const styles = StyleSheet.create({
